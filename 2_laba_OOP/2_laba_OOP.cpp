11322262c36af4afccf55c8cad5d141799279a1e
MenikUG <ivan2014159@gmail.com>
2020-10-23 11:59:32 +0500

    V0.0.1
    Создал класс для точки

---
+++ 2_laba_OOP/2_laba_OOP.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include "conio.h"
+
+using namespace std;
+
+class Point {
+public:
+    int x, y;
+
+    Point() {
+        x = 0;
+        y = 0;
+        printf("Запустился конструктор по умолчанию у класса Point\n");
+    }
+
+    Point(int x, int y) {
+        this->x = x;
+        this->y = y;
+        printf("Запустился конструктор c параметрами у класса Point\n");
+    }
+
+    Point(const Point& p) {
+        x = p.x;
+        y = p.y;
+        printf("Запустился конструктор копирования у класса Point\n");
+    }
+
+    ~Point() {
+        printf("Запустился деструктор у класса Point\n");
+    }
+};
+
+
+
+
+
+int main() {
+    setlocale(0, "");
+
+    printf("Point tochka;\n");
+    Point tochka;
+
+    printf("\nPoint tochka1(2, 5);\n");
+    Point tochka1(2, 5);
+
+    printf("\nPoint* tochka2 = new Point();\n");
+    Point* tochka2 = new Point();
+
+
+
+
+
+}
